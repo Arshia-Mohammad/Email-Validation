63a1eb94162b9d427253f3884606b82d4d5c5cc0
arshia <mohammadarshia431@gmail.com>
2025-07-28 16:06:12 +0530

    Add project files.

---
+++ EmailValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Email_Validation
+{
+    public class EmailValidator
+    {
+        public bool IsEmailValid (string email)
+        {
+            //[^@\s]=one or more characters that are not, @ or whitespace
+            //$ = end of the string
+            // Mobile no : @"^[6-9]\d{9}$"
+            if (string.IsNullOrWhiteSpace(email))return false;
+            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, pattern);
+
+        }
+    }
+}
